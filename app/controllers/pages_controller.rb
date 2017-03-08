e3c404afb4212b217c1850fd37d850cb091f2b14
Dariasz <drabekdariusz@gmail.com>
2017-03-08 16:47:29 +0100

    devise login via a nickname

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,2 @@
 class PagesController < ApplicationController
-  def index
-  end
-
-  def windows
-  end
-
-  def linux
-  end
-
-  def trivias
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(nickname) = :value OR lower(email) = :value", {:value => login.downcase}]).first
+    elsif conditions.has_key?(:nickname) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
 end
